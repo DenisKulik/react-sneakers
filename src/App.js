a6c1fb564a5144c6cddeb730cdc07022ddce94fa
Denis Kulik <kulikdenis1996x@gmail.com>
2023-03-20 22:22:55 +0700

    add header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 function App() {
     return (
-        <div className="App">
-            Hello, World!
+        <div className="wrapper">
+            <header className="header">
+                <div className="headerLeft">
+                    <img width={40} height={40}
+                         src={'/react-sneakers/img/logo.png'}
+                         alt="Logotype" />
+                    <div className="headerInfo">
+                        <h3>React Sneakers</h3>
+                        <p>Магазин лучших кроссовок</p>
+                    </div>
+                </div>
+
+                <ul className="headerRight">
+                    <li>
+                        <img width={18} height={18}
+                             src={'/react-sneakers/img/cart.svg'} alt="cart" />
+                        <span>1205 руб.</span>
+                    </li>
+                    <li>
+                        <img width={18} height={18}
+                             src={'/react-sneakers/img/user.svg'} alt="" />
+                    </li>
+                </ul>
+            </header>
         </div>
     );
 }
