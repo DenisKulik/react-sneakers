8528b942130d5191ad9a8b0fd705d136c4ebbf0b
Denis Kulik <kulikdenis1996x@gmail.com>
2023-03-23 21:32:04 +0700

    add Card module styles

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,22 +1,24 @@
+import styles from './Card.module.scss';
+
 function Card(props) {
     const { img, title, price } = props;
 
     return (
-        <div className="card">
-            <div className="favorite">
+        <div className={styles.card}>
+            <div className={styles.favorite}>
                 <img src={'/react-sneakers/img/unliked.svg'}
                      alt="Unliked" />
             </div>
-            <img className="cardImage" width={133}
+            <img className={styles.cardImage} width={133}
                  src={img}
                  alt={title} />
-            <h5 className="cardHeading">{title}</h5>
-            <div className="cardBottom">
-                <div className="cardPrice">
+            <h5 className={styles.cardHeading}>{title}</h5>
+            <div className={styles.cardBottom}>
+                <div className={styles.cardPrice}>
                     <span>Цена:</span>
                     <b>{price} руб.</b>
                 </div>
-                <button className="cardBtn">
+                <button className={styles.cardBtn}>
                     <img src={'/react-sneakers/img/btn-plus.svg'}
                          alt="Add to cart" />
                 </button>
